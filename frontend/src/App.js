e3cee0762c8e0a042702276def52e3c484186734
a-bashar-fanshawe <a_bashar@fanshaweonline.ca>
2026-02-20 18:04:51 -0500

    Add Address components, Programs components and brochure PDF

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import Header from "./components/Header/Header";
+import Address from "./components/Address/Address";
 import Footer from "./components/Footer/Footer";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <p>
         <div>Welcome to Onward Sun</div>
       </p>
+      <Address />
       <hr />
       <Footer />
     </>
---
+++ frontend/src/components/Programs/SchoolYearProgram/SchoolYearProgram.js
@@ -0,0 +1,86 @@
+function SchoolYearProgram() {
+  return (
+    <>
+      <h2>School Year Program</h2>
+
+      <h3>Social Skills Enrichment for Children</h3>
+
+      <p>
+        Developed to meet the needs of children aged 5-14 years. Presented in a
+        fun, hands-on, self-empowering format, designed...just for kids.
+      </p>
+
+      <h3>Social Skills Tutoring</h3>
+      <ul>
+        <li>Problem Solving</li>
+        <li>Stopping and thinking before you do</li>
+        <li>Social Interaction</li>
+        <li>Dealing with teasing</li>
+        <li>Feeling confident</li>
+        <li>Thinking of alternatives to aggression</li>
+        <li>Friendship skills</li>
+        <li>Communicating needs and feelings</li>
+      </ul>
+
+      <h3>Group Format</h3>
+      <ul>
+        <li>Small manageable groups</li>
+        <li>Evening groups, one hour in length, offered through the week</li>
+        <li>Weekly communication with parents</li>
+        <li>Techniques taught through art, play and creative expression</li>
+        <li>
+          Emphasis on self-worth, feeling identification, stopping and thinking
+          before you do
+        </li>
+        <li>A pro-social approach to teaching social skills</li>
+        <li>Groups conducted by one facilitator and group aides</li>
+        <li>
+          Regular groups continue throughout the year with the exception of
+          Christmas, Spring and Summer break
+        </li>
+        <li>
+          Small, weekly at home activity sheets encourage group concepts outside
+          the Onward program setting
+        </li>
+      </ul>
+
+      <h3>Goals</h3>
+      <ul>
+        <li>
+          For children to take responsibility for their lives, actions and
+          limitations
+        </li>
+        <li>
+          To provide a safe environment for children to discuss their concerns
+          and goals
+        </li>
+        <li>
+          To help the child recognize their positives and potential, enhancing
+          their self esteem
+        </li>
+        <li>
+          To learn specific ways of integrating the taught social skills into
+          their life
+        </li>
+        <li>
+          Children will become aware and comfortable with their emotions through
+          feeling identification
+        </li>
+        <li>
+          Children will learn to deal with their emotions in an empowering,
+          assertive way
+        </li>
+        <li>
+          For children to strengthen friendships both within and outside the
+          group
+        </li>
+      </ul>
+
+      <a href="/school-year-program.pdf" download>
+        Click here to download our School Year Brochure
+      </a>
+    </>
+  );
+}
+
+export default SchoolYearProgram;
