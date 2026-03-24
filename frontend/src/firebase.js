733ab133532007423bd6e1f17d10b977ba99a8c7
Groozma <r_maguire@fanshaweonline.ca>
2026-03-24 18:01:29 -0400

    Hiding firebase.js config

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -8,13 +8,15 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBfiuwaEDYNyyPqKWI3JTb6EIEK5tq-SH0",
-  authDomain: "onward-social-skills.firebaseapp.com",
-  projectId: "onward-social-skills",
-  storageBucket: "onward-social-skills.firebasestorage.app",
-  messagingSenderId: "839028325645",
-  appId: "1:839028325645:web:3995672cb04bc4cf3d4444"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
