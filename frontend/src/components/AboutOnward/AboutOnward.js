039a96c555e644044b843ab75460e86869feed6c
Groozma <r_maguire@fanshaweonline.ca>
2026-03-25 14:18:49 -0400

    Added the About Page

---
+++ frontend/src/components/AboutOnward/AboutOnward.js
@@ -0,0 +1,12 @@
+import './AboutOnward.scss'
+
+function AboutOnward() {
+    return (
+        <div className="aboutOnward">
+            <h3>About Onward</h3>
+            <p>Danielle Aziz; a certified member of the Child & Youth Worker Association; developed Onward in 1996. The program was developed, from what Danielle recognized was a need to educate and equip children who were having social difficulties. Danielle found that children exhibiting the characteristics of underdeveloped social skills were having great difficulty implementing the skills taught by conventional means. The Onward program teaches various skills, with an interactive component that makes these concepts real. Children that ordinarily have difficulty focusing or becoming involved in social skills programs, feel at ease and motivated to learn the concepts that Onward offers.</p>
+        </div>
+    )
+}
+
+export default AboutOnward;
---
+++ frontend/src/components/News/News.js
@@ -0,0 +1,17 @@
+import photo from '../../images/Danielle-and-Amie_1.jpg';
+import './News.scss';
+
+function News() {
+    return(
+        <div className="news">
+            <h3>In The News</h3>
+            <div className='flex-row'>
+                <img src={photo} alt='Two women standing side by side' className='news-photo'/>
+                <p>Onward has been featured in the Fanshawe College Alumni Newsletter.</p>
+            </div>
+            <a href='./Inthenews.pdf'>Click here to download and read the article in PDF format</a>
+        </div>
+    )
+}
+
+export default News;
